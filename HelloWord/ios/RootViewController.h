e2769f5220e1c895997c74252beff0b3effbab0b
admin <admin@bogon.(none)>
2013-03-01 16:20:25 +0800

    Initial Commit

---
+++ HelloWord/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  HelloWordAppController.h
+//  HelloWord
+//
+//  Created by admin on 13-3-1.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
